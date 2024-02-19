466041410dd681d881b0fb5040ea1e7d35715c6b
Mattias Fransson <mattias@webben.nu>
2024-02-19 21:10:58 +0100

    slice of day

--- veckomeny.py
+++ veckomeny.py
@@ -8,4 +8,21 @@ html = r.text
 soup = BeautifulSoup(html, 'html.parser')
 div = soup.find('div', class_="week visible")
 
-print(f"{div.get_text()}")
+lines = [ x for x in div.get_text().split('\n') 
+          if not (
+            x == '' or
+            x == 'Mån' or
+            x == 'Tis' or
+            x == 'Ons' or
+            x == 'Tor' or
+            x == 'Fre'
+          )
+        ]
+
+mon_i = lines.index('Måndag')
+tue_i = lines.index('Tisdag')
+wed_i = lines.index('Onsdag')
+thu_i = lines.index('Torsdag')
+fri_i = lines.index('Fredag')
+
+print(f"{lines[mon_i:tue_i]}")
